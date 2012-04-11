492416c852c18907e108c1093d4982ce9eb2d201
Michael Alexander <mlalexander@LM125890-MLALE.(none)>
2012-04-10 17:55:02 -0700

    Initial commit.

---
+++ ex1.9.scm
@@ -0,0 +1,20 @@
+; Iterative procedure
+; 
+;(define (+ a b)
+;  (if (= a 0)
+;    b
+;    (inc (+ (dec a) b))))
+;
+; evaluate (+ 4 5) using the substituion model
+;
+;  (if (= 4 0)
+;    5
+;    (inc (+ (dec 4) 5))))
+
+
+; Recursive procecure
+;
+;(define (+ a b)
+;  (if (= a 0)
+;    b
+;    (+ (dec a) (inc b))))
