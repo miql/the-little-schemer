842cae70e8ebd22fcaa1d0bb033db9e83452ea7b
Michael Alexander <mlalexander@LM125890-MLALE.(none)>
2012-10-11 23:51:21 -0700

    Up to Fourth Commandment

--- little.scm
+++ little.scm
@@ -25,3 +25,21 @@
       (else (cond
               ((eq? (car lat) a) (cdr lat))
               (else (cons (car lat) (rember a lat))))))))
+
+(define firsts
+  (lambda (l)
+    (cond 
+      ((null? l) (quote ()))
+      (else (cons (car (car l) (firsts (cdr l))))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? (car lat) old)
+               (cons old
+                     (cons new (cdr lat))))
+              (else (cons (car lat)
+                          (insertR new old
+                                   (cdr lat)))))))))
