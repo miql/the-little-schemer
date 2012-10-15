4a001612184d3863b3664f44e67bd6293f40d278
Michael Alexander <mlalexander@LM125890-MLALE.(none)>
2012-10-14 22:15:42 -0700

    Including substitute functions

--- little.scm
+++ little.scm
@@ -43,3 +43,24 @@
               (else (cons (car lat)
                           (insertR new old
                                    (cdr lat)))))))))
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? (car lat) old)
+               (cons new 
+                     (cons old (cdr lat))))
+              (else (cons (car lat)
+                          (insertL new old
+                                   (cdr lat)))))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((eq? (car lat) old)
+               (cons new (cdr lat)))
+              (else (cons (car lat) (subst new old (cdr lat)))))))))
+
