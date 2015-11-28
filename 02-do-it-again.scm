834d2a2307d40a7ab76e4d477d65896d39d2345e
miql <mxander@gmail.com>
2015-11-28 00:11:10 -0700

    Start lat? and commandments in README

--- 02-do-it-again.scm
+++ 02-do-it-again.scm
@@ -1,4 +1,18 @@
 ; #t or #f:
-; (lat? '(Jack Sprat could eat no chicken fat))
+(lat? '(Jack Sprat could eat no chicken fat))
 ; #t, because each S-expression in the list is an atom.
 
+(lat? '((Jack) Sprat could eat no chicken fat))
+; #f, since the first S-Expression is a list
+
+(lat? '())
+; #t, because it does not contain a list
+
+; lat is a lits of atoms
+
+(define lat?
+  (lambda (l)
+     (cond          ; let's ask some questions
+     ((null? l) #t) ; is this a null list?
+     ((atom? (car l) (lat? (cdr l)))) ; is the first S-expression in the list an atom? is the rest an atom?
+     (else #f))))   ; otherwise, it's not a list of atoms
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ The-Little-Schemer
 ==================
 
 Examples from The Little Schemer
+
+## The First Commandment
+
+Always ask `null?` as the first question in expressing any functions.
