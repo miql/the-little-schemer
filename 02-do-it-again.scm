bd4c9e8fd10568ff3c02fcf6976e0dbf6ab40fc8
miql <mxander@gmail.com>
2015-11-28 23:29:56 -0700

    Completed 2nd chapter

--- 02-do-it-again.scm
+++ 02-do-it-again.scm
@@ -16,3 +16,22 @@
        ((null? l) #t) ; is this an empty list?
        ((atom? (car l) (lat? (cdr l)))) ; is the first S-expression in the list an atom? is the rest an atom?
        (else #f))))   ; otherwise, it's not a list of atoms
+
+; is this true?
+(or (null? '())
+   (atom? '(d e f g)))
+; #t
+
+; #t or #f?
+(or (null? '(a b c))
+    (null? '(atom)))
+
+; `or` asks questions one at a time
+
+(define member?
+  (lambda (a lat)
+    (cond
+     ((null? lat) #f) ; asks if lat is a null list, if so, a is not a member
+     (else ; no other questions to ask
+      (or (eq? (car lat) a) ; since lat is not null, let's find out if `car` of `lat` is the same as `a` or in the rest of `lat`
+          (member? a (cdr lat)))))))
