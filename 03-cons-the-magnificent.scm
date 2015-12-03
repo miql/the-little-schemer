ea1ea0419be78ea5bc98ae9abfc60ca27a63b36e
miql <mxander@gmail.com>
2015-12-02 21:39:43 -0700

    Add 3rd commandment & insertR

--- 03-cons-the-magnificent.scm
+++ 03-cons-the-magnificent.scm
@@ -17,3 +17,45 @@
 
 '(a c e)
 
+; what is (firsts '())
+'()
+
+; what is (firsts ((five plums)
+;                  (four)
+;                  (eleven green oranges))
+
+; what is (first (((five plums) four)
+;                  (eleven green oranges)
+;                  ((no) more))
+
+'((five plums) eleven (no))
+
+; builds another list composed of the first S-expression of each internal list
+(define firsts
+  (lambda (l)
+    (cond ; because we need to ask questions about the arguments
+     ((null? l) '()) ; The First Commandment
+     (else (cons (car (car l) ; Because we only have 2 questions to ask about `l` (null | non-empty)
+                 (firsts (cdr l)))))))
+
+; what is (insertR 'topping 'fudge '(ice cream with fudge for dessert)) ?
+'(ice cream with fudge topping for dessert)
+
+; (insertR 'jalepeño 'and '(taco tamales and salsa)) ?
+'(taco tamales and jalepeño salsa)
+
+; (insertR 'e 'd '(a b c d f g h))
+'(a b c d e f g h)
+
+; takes 3 args: the atoms `new`, `old`, and `lat`.
+; the function insertR builds a lat with `new` inserted to the right
+; of the first occurrence of `old`
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     (cond
+      ((eq? old (car lat))
+       (cons new (cdr lat)))
+      (else (cons (car lat
+                       (insertR new old (cdr lat)))))))))
--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Always ask `null?` as the first question in expressing any functions.
 ## The Second Commandment
 
 Use `cons` to build lists.
+
+## The Third Commandment
+
+When building a list, describe the first typical element, and then `cons` it onto the natural recursion.
