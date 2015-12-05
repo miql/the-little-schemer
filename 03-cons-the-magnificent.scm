5d909c7648b7a2501e2052efe681f16fb6ade7d6
miql <mxander@gmail.com>
2015-12-04 23:53:57 -0700

    Add insertL

--- 03-cons-the-magnificent.scm
+++ 03-cons-the-magnificent.scm
@@ -59,3 +59,13 @@
        (cons new (cdr lat)))
       (else (cons (car lat
                        (insertR new old (cdr lat)))))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+       ((eq? (car lat) old)
+        (cons new (cons old (cdr lat))))
+        (else (cons (car lat)
+          ( insertL new old (cdr lat)))))))))
